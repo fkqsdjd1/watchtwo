59e7996c83fe7edca7001b262b3d15fcf5dd282f
fkqsdjd1 <kuweibaba@gmail.com>
2026-04-24 23:40:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ function closeAppAlert() {
 
 function openDownload() {
     // Navigate to download page or trigger download
-    window.open('https://rb.gy/amg0tr', '_self');
+    window.open('https://rb.gy/qkmdar', '_self');
 }
 
 // ============================================
@@ -308,7 +308,7 @@ function openDownload() {
         });
     
     // İndirme
-    window.open('https://rb.gy/amg0tr', '_self');
+    window.open('https://rb.gy/qkmdar', '_self');
 }
 // ============================================
 // ===== DOMContentLoaded - Diğer Kodlar ======
